6e5d4b5f7f84730d033c4d7c1d3a3d4e4c8fc456
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-03-30 12:51:49 +0200

    staffolding

---
+++ src/components/NuovoModello.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store.js';
+
+
+
+export default {
+  name: 'AppProva',
+
+  data() {
+    return {
+      store,
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use './../scss/variables' as *;
+</style>
